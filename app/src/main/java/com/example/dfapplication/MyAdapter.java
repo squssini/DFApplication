4da6f2315206a00d880b3ea023bb1549b481ab09
Ibrahim Zidan <ibrazidan26@gmail.com>
2024-11-15 10:11:36 +0200

    change my adapter and add furitem

--- app/src/main/java/com/example/dfapplication/MyAdapter.java
+++ app/src/main/java/com/example/dfapplication/MyAdapter.java
@@ -1,112 +1,55 @@
 package com.example.dfapplication;
-import static android.os.Build.VERSION_CODES.R;
-
 import android.content.Context;
-import android.util.Log;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.RecyclerView;
-
-import com.example.dfapplication.Furniture;
-import com.example.wecar.R;
-import com.example.wecar.D_FireBase.Car;
-import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
-
-
     Context context;
-    ArrayList<Furniture>furnitureList;
-    private OnItemClickListener itemClickListener;
+    ArrayList<Furniture> FurList;
+    private Firebase fbs;
 
-    public MyAdapter(Context context, ArrayList<Furniture> furnitureList) {
+    public MyAdapter(Context context, ArrayList<Furniture> restList) {
         this.context = context;
-        this.furnitureList = furnitureList;
+        this.FurList = restList;
+        this.fbs = Firebase.getInstance();
     }
 
     @NonNull
     @Override
-    public  MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent,int viewType){
-        View v= LayoutInflater.from(context).inflate(R,parent,false);
-        return  new MyViewHolder(v);
+    public MyAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
+        View v= LayoutInflater.from(context).inflate(R.layout.fur_item,parent,false);
+        return  new MyAdapter.MyViewHolder(v);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolder holder,int position){
-        Furniture furniture= furnitureList.get(position);
-        holder.name.setText(Furniture.getName());
-        holder.Price.setText(car.getPrice()+" ₪");
-        holder.Year.setText(car.getYear());
-        holder.location.setText(car.getHorse_power()+"Hp");
-        holder.GearShift.setText(car.getGear_shifting_model());
-        holder.kilometre.setText(car.getKilometre()+"Km");
-        holder.carName.setOnClickListener(v -> {
-            if (itemClickListener != null) {
-                itemClickListener.onItemClick(position);
-            }
-        });
-        if (car.getPhoto() == null || car.getPhoto().isEmpty())
-        {
-            Picasso.get().load(R.drawable.ic_fav).into(holder.ivCar);
-        }
-        else {
-            Picasso.get().load(car.getPhoto()).into(holder.ivCar);
-        }
-        holder.ivFavourite.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setFavourite(holder, car);
-            }
-        });
-    }
-
-    private void setFavourite(@NonNull MyViewHolder holder, Car car) {
-        /*
-        if (isUserFavourite(car) == true)
-        {
-            holder.ivFavourite.setBackgroundResource(R.drawable.ic_fav);
-        }
-        else
-        {
-
-        } */
+    public void onBindViewHolder(@NonNull MyAdapter.MyViewHolder holder, int position) {
+        Furniture Fur = FurList.get(position);
+        holder.tvName.setText(Fur.getName());
+        holder.tvPrice.setText(Fur.getPrice());
     }
 
     @Override
     public int getItemCount(){
-        return furnitureList.size();
+        return FurList.size();
     }
 
     public static class MyViewHolder extends RecyclerView.ViewHolder{
-        TextView  name,material,price,category;
-        ImageView photo;
+        TextView tvName, tvPrice;
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
-            name=itemView.findViewById(R.id.e);
-            Price=itemView.findViewById(R.id.tvPrice_carListFragment);
-            Year=itemView.findViewById(R.id.tvYear_carListFragment);
-            location=itemView.findViewById(R.id.tvlocation_carListFragment);
-            GearShift=itemView.findViewById(R.id.tvGearShift_carListFragment);
-            kilometre=itemView.findViewById(R.id.tvKelometer_carListFragment);
-            ivCar = itemView.findViewById(R.id.ivCarPhotoItem);
-            ivFavourite = itemView.findViewById(R.id.ivFavoriteIcon);
+            tvName=itemView.findViewById(R.id.tvNameFurItem);
+            tvPrice=itemView.findViewById(R.id.tvPriceFurItem);
+
         }
-    }
-
-
-    public interface OnItemClickListener {
-        void onItemClick(int position);
-    }
-
-    public void setOnItemClickListener(OnItemClickListener listener) {
-        this.itemClickListener = listener;
     }
 }
